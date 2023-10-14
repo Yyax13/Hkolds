ee4e65b3aed0a9e5f93665bd69f907fe5be69c89
Yyax13 <138953949+Yyax13@users.noreply.github.com>
2023-10-14 20:14:11 +0000

    In development message

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,10 @@ function Home() {
     return <html lang="pt-br">
         <head>
             <title>HK Olds</title>
-            <link rel="icon" type="png" href="" />
         </head>
         <body>
             <h1>Hkolds</h1>
-            <p>Retaking the old forum, but now, is for devs</p>
-            <p>check the github page <a href="https://github.com/Yyax13/Hkolds">here</a></p>
+            <p>In development ...</p>
         </body>
     </html>
 }
