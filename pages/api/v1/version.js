51cae127ba424481a88e296fcb515497496114c2
Yyax13 <138953949+Yyax13@users.noreply.github.com>
2023-10-19 17:44:20 +0000

    API v1 version correct error

--- pages/api/v1/version.js
+++ pages/api/v1/version.js
@@ -1,19 +1,17 @@
 import fs from 'fs';
-import path from 'path'
+import path from 'path';
 
 export default (request, response) => {
-    const packageJsonPath = path.join(process.cwd(), '../../../package.json');
+    const packageJsonPath = path.join(process.cwd(), 'package.json'); // Caminho absoluto para o package.json
+
     try {
-        const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
+        const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
         const version = packageJson.version;
-        response.status(200).json(
-            {
-                status: "Deploy",
-                projectVersion: `${version}`
-            }
-        );
+        response.status(200).json({
+            status: 'Deploy',
+            projectVersion: `${version}`,
+        });
     } catch (error) {
-        response.status(500).json({error: "ERROR"})
+        response.status(500).json({ error: 'ERROR' });
     }
-        
 };
